4661987735dc66579403ee036dd52009c2848071
hawke <blue2011sky@foxmail.com>
2022-06-04 15:59:38 +0800

    创建了index首页和退出功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,9 +1,21 @@
 // 每次调用$.get $.post() $.ajax()的时候，会先调用ajaxPrefilter（）这个函数
 // 在这个函数中，我们可以拿到 我们给ajax提供的配置对象options
 $.ajaxPrefilter(function (options) {
-    console.log(options);
     // 在真正发起ajax请求前， 统一拼接请求的url
     options.url = 'http://www.liulongbin.top:3008/' + options.url;
+    // 统一为有权限的接口，设置headers请求头, 如果url中包含了/my/则需要添加headers
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = { Authorization: localStorage.getItem('token') || '' }
+    }
     console.log(options);
-
+    // 全局统一挂载 complete 回调函数
+    // 无论成功还是失败，ajax都会调用 complete 回调函数
+    options.complete = function (res) {
+        if (res.responseJSON.code === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 强制清空本地token数据
+            localStorage.removeItem('token');
+            // 强制跳转页面
+            location.href = 'login.html';
+        }
+    }
 }) 
---
+++ assets/js/index.js
@@ -0,0 +1,56 @@
+$(function () {
+    getUserInfo();
+
+    // 退出登陆
+    $('#btnLogout').on('click', function () {
+        layui.layer.confirm('确定退出登陆?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            // 清空本地存储的token数据
+            localStorage.removeItem('token');
+            // 跳转到login页面
+            location.href = 'login.html';
+            layer.close(index);
+        });
+    })
+})
+
+// 获取用户信息函数
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: 'my/userinfo',
+        success: function (res) {
+            console.log(res)
+            if (res.code !== 0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+            renderAvatar(res.data);
+        }
+        // 无论成功还是失败，ajax都会调用 complete 回调函数
+        // complete: function (res) {
+        //     if (res.responseJSON.code === 1 && res.responseJSON.message === '身份认证失败！') {
+        //         // 强制清空本地token数据
+        //         localStorage.removeItem('token');
+        //         // 强制跳转页面
+        //         location.href = 'login.html';
+        //     }
+        // }
+    })
+}
+
+// 头像函数
+function renderAvatar(user) {
+    // 获取用户名称
+    var name = user.nickname || user.username;
+    // 渲染用户名
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+    // 如果没有头像, 则使用文字头像, 
+    if (user.user_pic !== null) {
+        // 如有有头像，则隐藏文字头像
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide()
+    } else {
+        $('.text-avatar').html(name[0].toUpperCase()).show();
+        $('.layui-nav-img').hide();
+    }
+}
